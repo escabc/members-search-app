7ba0074801b9615cf0942a06c672415c38b2d52f
Gabrielle Carson <gabriellecarson@Gabrielles-Macbook.local>
2021-08-04 13:33:19 -0700

    Remove react-tap-event-plugin.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import FastClick from 'fastclick'
-import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import App from './App'
 
@@ -12,10 +11,6 @@ import App from './App'
 // and the firing of a click event on mobile browsers
 // https://github.com/ftlabs/fastclick
 FastClick.attach(document.body)
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin()
 
 const mount = document.getElementById('root')
 
